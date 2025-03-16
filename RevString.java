d945e17a75b50be5e3d3f23db83491e8d10ae458
jayaraman7 <167000510+jayaraman7@users.noreply.github.com>
2025-03-16 14:06:17 +0530

    RevString.java

---
+++ RevString.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String s ="Hello";
+        String rev ="";
+        int len = s.length();
+        for(int i=len-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
