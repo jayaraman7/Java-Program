9324e4a85a068234799f05313a3299f3e4a0bbd2
jayaraman7 <167000510+jayaraman7@users.noreply.github.com>
2025-03-16 14:20:14 +0530

    SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int num = 123;
+        int sum = 0;
+        while(num>0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        System.out.println(sum);
+        
+    }
+} 
